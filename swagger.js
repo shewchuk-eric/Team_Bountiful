d886a3cbb0eb1e94b9b371e60eb635fc3afc91fe
Eric Shewchuk <temeculastoneworks@msn.com>
2025-11-11 20:40:05 -1000

    adjustment to swagger build doc to accomodate Render deploy

--- swagger.js
+++ swagger.js
@@ -5,7 +5,8 @@ const doc = {
     title: 'Book of Mormon Characters API',
     description: 'A searchable database of characters found in the Book of Mormon.',
   },
-  host: 'localhost:3000'
+  host: 'team-bountiful.onrender.com', // Local development = 'localhost:3000' Remote =  'team-bountiful.onrender.com'
+  schemes: ['https'], // Local development = 'http' Remote = 'https
 };
 
 const outputFile = './swagger.json';
