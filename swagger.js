639fe5772599fc9a7f8b2e9f1b36706e7ae0fc1f
Eric Shewchuk <temeculastoneworks@msn.com>
2025-11-19 10:35:21 -1000

    update swagger to use both local and remote schemes

--- swagger.js
+++ swagger.js
@@ -5,8 +5,19 @@ const doc = {
     title: 'Book of Mormon Characters API',
     description: 'A searchable database of characters found in the Book of Mormon.',
   },
-  host: 'localhost:3000', // Local development = 'localhost:3000' Remote =  'team-bountiful.onrender.com'
-  schemes: ['http'], // Local development = 'http' Remote = 'https
+  "host": "team-bountiful.onrender.com",
+  "servers": [
+    {
+      "url": "team-bountiful.onrender.com",
+      "description": "Render site",
+      "schemes": ["https"]
+    },
+    {
+      "url": "localhost:3000",
+      "description": "Localhost",
+      "schemes": ["http"]
+    }
+  ]
 };
 
 const outputFile = './swagger.json';
@@ -14,3 +25,6 @@ const routes = ['./routes/index.js'];
 
 
 swaggerAutogen(outputFile, routes, doc);
+
+/*host: 'localhost:3000', // Local development = 'localhost:3000' Remote =  'team-bountiful.onrender.com'
+  schemes: ['http'], // Local development = 'http' Remote = 'https'*/
