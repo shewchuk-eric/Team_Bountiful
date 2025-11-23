e2408e409353b8fdf28dca0e74f3a1e3932fadcc
DallinS <dallinstephens1@gmail.com>
2025-11-22 21:26:53 -0700

    Fixed swagger.js so that api-docs testing endpoints works for Render and localhost

--- swagger.js
+++ swagger.js
@@ -18,19 +18,20 @@ const doc = {
       "schemes": ["http"]
     }
   ], */
-  // host: 'team-bountiful.onrender.com',
-  servers: [
-    {
-      url: 'team-bountiful.onrender.com',
-      description: 'Render site',
-      schemes: ['https']
-    },
-    {
-      url: 'localhost:3000',
-      description: 'Localhost',
-      schemes: ['http']
-    }
-  ],
+  host: 'team-bountiful.onrender.com',
+  schemes: ['https', 'http'],
+  // servers: [
+  //   {
+  //     url: 'https://team-bountiful.onrender.com',
+  //     description: 'Render site',
+  //     schemes: ['https']
+  //   },
+  //   {
+  //     url: 'http://localhost:3000',
+  //     description: 'Localhost',
+  //     schemes: ['http']
+  //   }
+  // ],
   definitions: {
     Character: {
       _id: '6912970eb69127d1966091e3',
