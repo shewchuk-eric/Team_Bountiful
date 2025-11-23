a268abcc877224cbe3af6dc72eedf5fa7d9b316e
DallinS <dallinstephens1@gmail.com>
2025-11-22 20:24:46 -0700

    Added swagger documentation, validation, and wrote functions for controller con_images.js.

--- routes/images.js
+++ routes/images.js
@@ -3,8 +3,9 @@ const imagesController = require('../controllers/con_images.js');
 
 
 images.get('/', imagesController.listAll); 
-images.get('/:id/', imagesController.listByCharacter);
+// images.get('/:id/', imagesController.listByCharacter);
 images.get('/imageByBook/:book', imagesController.listByBook);
+images.get('/listByCharacter/:character', imagesController.listByCharacter);
 images.post('/createNewImage', imagesController.createNewImage);
 images.put('/updateImage/:id', imagesController.updateImage);
 images.delete('/removeImage/:id', imagesController.removeImage);
