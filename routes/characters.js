154c30d2321e5dc8b478e81b884485c84a820e6d
Hyrum Whittier <whittierht@gmail.com>
2025-11-24 16:14:32 -0700

    fixed order in routes

--- routes/characters.js
+++ routes/characters.js
@@ -1,14 +1,12 @@
 const characters = require('express').Router();
 const charactersController = require('../controllers/con_characters.js');
 
-
-characters.get('/', charactersController.listAll);
-characters.get('/:id', charactersController.listDetails);
 characters.get('/listByBook/:book', charactersController.listByBook);
 characters.get('/listByQuality/:quality', charactersController.listByQuality);
+characters.get('/:id', charactersController.listDetails);
+characters.get('/', charactersController.listAll);
 characters.post('/createNewCharacter', charactersController.createNewCharacter);
 characters.put('/updateCharacter/:id', charactersController.updateCharacter);
 characters.delete('/deleteCharacter/:id', charactersController.deleteCharacter);
-
 
 module.exports = characters;
--- routes/quotes.js
+++ routes/quotes.js
@@ -1,12 +1,10 @@
 const quotes = require('express').Router();
 const quotesController = require('../controllers/con_quotes.js');
 
-
 quotes.get('/', quotesController.listAll);
 quotes.get('/listByBook/:book', quotesController.listByBook);
 quotes.get('/listByCharacter/:id', quotesController.listByCharacter);
 quotes.post('/createNewQuote', quotesController.createNewQuote);
 quotes.put('/updateQuote/:id', quotesController.updateQuote);
-
 
 module.exports = quotes;
